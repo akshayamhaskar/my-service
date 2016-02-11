2ddffcabb107edcbc6afdf376cc12563645e9d9b
vaibhavchaudhari <work.vaibhav26@gmail.com>
2016-02-11 18:22:34 +0530

    myproject

---
+++ my service/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace my_service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+
+
+#if DEBUG
+            Service1 myservice = new Service1();
+            myservice.ondebug();
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+        }
+             private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+
+    }
+    }
+
---
+++ my service/Service1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace my_service
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void ondebug()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "on start creted.text");
+            //      System.Diagnostics.Process.Start(@"E:\PROJECT XL\INI SQLLOADER\ConsoleApplication2\ConsoleApplication2\ConsoleApplication2\bin\Debug\ConsoleApplication2.exe");
+            System.Diagnostics.Process.Start(@"C:\HealthDiagnostic\timothy_test.exe");
+        }
+
+        protected override void OnStop()
+        {
+            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "on stop creted.text");
+
+        }
+
+    }
+}
